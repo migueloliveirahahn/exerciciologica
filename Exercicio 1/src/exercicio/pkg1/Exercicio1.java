72ba82b9bf425a38f1425d6cd5b33b49f393e08b
migueloliveirahahn <hahnmiguel26@gmail.com>
2025-05-15 15:43:39 -0300

    exercicio 1

--- Exercicio 1/src/exercicio/pkg1/Exercicio1.java
+++ Exercicio 1/src/exercicio/pkg1/Exercicio1.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicio.pkg1;
 
-/**
- *
- * @author aluno.saolucas
- */
-public class Exercicio1 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Exercicio1 {
+    
+    public static boolean Perfeito(int num) {
+    int soma = 0;
+     for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                soma += i;
+            }
+        }
+        
+        
+        return soma == num;
+    }
+
+
     public static void main(String[] args) {
-        // TODO code application logic here
+          Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Digite um número inteiro para verificar se é perfeito:");
+        int numero = ler.nextInt();
+
+        
+        if (Perfeito(numero)) {
+            System.out.println(numero + " é um número perfeito.");
+        } else {
+            System.out.println(numero + " não é um número perfeito.");
+        }  
     }
     
 }
