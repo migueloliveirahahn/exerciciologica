21e73b11145c776306c39d366e3b83bc75c4c002
migueloliveirahahn <hahnmiguel26@gmail.com>
2025-05-16 15:02:48 -0300

    Update Exercicio2.java

--- Exercicio 2/src/exercicio/pkg2/Exercicio2.java
+++ Exercicio 2/src/exercicio/pkg2/Exercicio2.java
@@ -12,17 +12,38 @@ import java.util.Scanner;
  * @author aluno.saolucas
  */
 public class Exercicio2 {
+     public static String substituirVogais(String texto) {
+   char[] caracteres = new char[texto.length()];
+   
+    for (int i = 0; i < texto.length(); i++) {
+    char c = texto.charAt(i);
+   if (c == 'a' || c == 'A' ||
+       c == 'e' || c == 'E' ||
+       c == 'i' || c == 'I' ||
+       c == 'o' || c == 'O' ||
+       c == 'u' || c == 'U') { 
+       caracteres[i] = '*';
+   } else {
+       caracteres [i] = c;
+   }
+}
     
-    
-
+   String resultado = "";
+  for (int i = 0; i < caracteres.length; i++) { {
+    resultado += caracteres[i];
+}
+   return resultado;
+}
+    }
     public static void main(String[] args) {
         
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite uma frase:");
+        String entrada = ler.nextLine();
+
+        String modificada = substituirVogais(entrada);
+
+        System.out.println("Frase com vogais substituídas: " + modificada);
+        ler.close();
     }
 }
-
-
-
-    
-
-
-
